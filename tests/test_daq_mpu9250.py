c52623d58a54e00de306aba31faf41fc81f4da50
pi <philipp_gehring@yahoo.de>
2019-09-14 21:20:37 +0000

    debug test script

--- tests/test_daq_mpu9250.py
+++ tests/test_daq_mpu9250.py
@@ -1,6 +1,6 @@
-import sys, getopt  
- 
-sys.path.append('.')  
+import sys, getopt
+
+sys.path.append('.')
 import RTIMU  
 import os.path  
 import time  
@@ -24,16 +24,16 @@ t_fail_timer = 0.0
 t_shutdown = 0  
  
 if (not imu.IMUInit()):  
- hack = time.time()  
- imu_sentence = "$IIXDR,IMU_FAILED_TO_INITIALIZE*7C"  
- if (hack - t_print) > 1.0:  
-	 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-	 sock.sendto(imu_sentence, (IMU_IP, IMU_PORT))  
-	 t_print = hack  
-	 t_shutdown += 1  
-	 if t_shutdown > 9:  
-		 sys.exit(1)  
- 
+	hack = time.time()  
+ 	imu_sentence = "$IIXDR,IMU_FAILED_TO_INITIALIZE*7C"  
+	if (hack - t_print) > 1.0:  
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+		sock.sendto(imu_sentence, (IMU_IP, IMU_PORT))  
+		t_print = hack  
+		t_shutdown += 1  
+		if t_shutdown > 9:  
+			sys.exit(1)  
+
 imu.setSlerpPower(0.02)  
 imu.setGyroEnable(True)  
 imu.setAccelEnable(True)  
@@ -74,7 +74,6 @@ hack = time.time()
  
 # if it's been longer than 5 seconds since last print  
 if (hack - t_damp) > 5.0:  
-		
 	if (hack - t_fail) > 1.0:  
 		t_one = 0  
 		t_three = 0  
@@ -96,66 +95,66 @@ if (hack - t_damp) > 5.0:
 		t_shutdown += 1  
  
 if imu.IMURead():  
- data = imu.getIMUData()  
- fusionPose = data["fusionPose"]  
- Gyro = data["gyro"]  
- t_fail_timer = 0.0  
+	data = imu.getIMUData()  
+	fusionPose = data["fusionPose"]  
+	Gyro = data["gyro"]  
+	t_fail_timer = 0.0  
  
- if (hack - t_damp) > .1:  
-	 roll = round(math.degrees(fusionPose[0]), 1)  
-	 pitch = round(math.degrees(fusionPose[1]), 1)  
-	 yaw = round(math.degrees(fusionPose[2]), 1)  
-	 rollrate = round(math.degrees(Gyro[0]), 1)  
-	 pitchrate = round(math.degrees(Gyro[1]), 1)  
-	 yawrate = round(math.degrees(Gyro[2]), 1)  
-	 if yaw < 90.1:  
-		 heading = yaw + 270 - magnetic_deviation  
-	 else:  
-		 heading = yaw - 90 - magnetic_deviation  
-	 if heading > 360.0:  
-		 heading = heading - 360.0  
+if (hack - t_damp) > .1:  
+	roll = round(math.degrees(fusionPose[0]), 1)  
+	pitch = round(math.degrees(fusionPose[1]), 1)  
+	yaw = round(math.degrees(fusionPose[2]), 1)  
+	rollrate = round(math.degrees(Gyro[0]), 1)  
+	pitchrate = round(math.degrees(Gyro[1]), 1)  
+	yawrate = round(math.degrees(Gyro[2]), 1)  
+	if yaw < 90.1:  
+		heading = yaw + 270 - magnetic_deviation  
+	else:  
+		heading = yaw - 90 - magnetic_deviation  
+	if heading > 360.0:  
+		heading = heading - 360.0  
 			 
-	 # Dampening functions  
-	 roll_total = roll_total - roll_run[t_one]  
-	 roll_run[t_one] = roll  
-	 roll_total = roll_total + roll_run[t_one]  
-	 roll = roll_total / 10  
-	 heading_cos_total = heading_cos_total - heading_cos_run[t_three]  
-	 heading_sin_total = heading_sin_total - heading_sin_run[t_three]  
-	 heading_cos_run[t_three] = math.cos(math.radians(heading))  
-	 heading_sin_run[t_three] = math.sin(math.radians(heading))  
-	 heading_cos_total = heading_cos_total + heading_cos_run[t_three]  
-	 heading_sin_total = heading_sin_total + heading_sin_run[t_three]  
-	 heading = round(math.degrees(math.atan2(heading_sin_total/30,heading_cos_total/30)),1)  
-	 if heading < 0.1:  
-		 heading = heading + 360.0  
+	# Dampening functions  
+	roll_total = roll_total - roll_run[t_one]  
+	roll_run[t_one] = roll  
+	roll_total = roll_total + roll_run[t_one]  
+	roll = roll_total / 10  
+	heading_cos_total = heading_cos_total - heading_cos_run[t_three]  
+	heading_sin_total = heading_sin_total - heading_sin_run[t_three]  
+	heading_cos_run[t_three] = math.cos(math.radians(heading))  
+	heading_sin_run[t_three] = math.sin(math.radians(heading))  
+	heading_cos_total = heading_cos_total + heading_cos_run[t_three]  
+	heading_sin_total = heading_sin_total + heading_sin_run[t_three]  
+	heading = round(math.degrees(math.atan2(heading_sin_total/30,heading_cos_total/30)),1)  
+	if heading < 0.1:  
+		heading = heading + 360.0  
  
 	 t_damp = hack  
 	 t_one += 1  
-	 if t_one == 10:  
-		 t_one = 0  
-	 t_three += 1  
-	 if t_three == 30:  
-		 t_three = 0  
+	if t_one == 10:  
+		t_one = 0  
+	t_three += 1  
+	if t_three == 30:  
+		t_three = 0  
 	
-	 if (hack - t_print) > 1:  
-		 hdt = "IIHDT," + str(round(heading))[:-2] + ",T"  
-		 hdtcs = format(reduce(operator.xor,map(ord,hdt),0),'X')  
-		 if len(hdtcs) == 1:  
-			 hdtcs = "0" + hdtcs  
-		 iihdt = "$" + hdt + "*" + hdtcs  
+	if (hack - t_print) > 1:  
+		hdt = "IIHDT," + str(round(heading))[:-2] + ",T"  
+		hdtcs = format(reduce(operator.xor,map(ord,hdt),0),'X')  
+		if len(hdtcs) == 1:  
+			hdtcs = "0" + hdtcs  
+		iihdt = "$" + hdt + "*" + hdtcs  
+		
+		xdr = "IIXDR,A," + str(int(round(roll))) + ",D,ROLL,A," + str(int(round(pitch))) + ",D,PTCH,A," + str(int(round(rollrate))) + ",D,RLLR,A," + str(int(round(pitchrate))) + ",D,PTCR,A," + str(int(round(yawrate))) + ",D,YAWR"  
+		xdrcs = format(reduce(operator.xor,map(ord,xdr),0),'X')  
+		if len(xdrcs) == 1:  
+			xdrcs = "0" + xdrcs  
+		iixdr = "$" + xdr + "*" + xdrcs  
+ 
+		imu_sentence = iihdt + '\r\n' + iixdr  
+ 
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+		sock.sendto(imu_sentence, (IMU_IP, IMU_PORT))  
+ 
+		t_print = hack  
 		 
-		 xdr = "IIXDR,A," + str(int(round(roll))) + ",D,ROLL,A," + str(int(round(pitch))) + ",D,PTCH,A," + str(int(round(rollrate))) + ",D,RLLR,A," + str(int(round(pitchrate))) + ",D,PTCR,A," + str(int(round(yawrate))) + ",D,YAWR"  
-		 xdrcs = format(reduce(operator.xor,map(ord,xdr),0),'X')  
-		 if len(xdrcs) == 1:  
-			 xdrcs = "0" + xdrcs  
-		 iixdr = "$" + xdr + "*" + xdrcs  
- 
-		 imu_sentence = iihdt + '\r\n' + iixdr  
- 
-		 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-		 sock.sendto(imu_sentence, (IMU_IP, IMU_PORT))  
- 
-		 t_print = hack  
-		 
- time.sleep(poll_interval*1.0/1000.0) 
+time.sleep(poll_interval*1.0/1000.0) 
