32d423917f0fe3cb9c565636d9dff7611a107bc1
phil4991 <philipp_gehring@yahoo.de>
2019-09-15 21:05:04 +0200

    implemented command button

--- sb-robot/core/helpers.py
+++ sb-robot/core/helpers.py
@@ -20,26 +20,43 @@ class Configuration:
 class IO():
 	def __init__(self):
 		self._pins = []
+		self.event_status = False
+
+		self._button_id = 0
 
 	def configure(self, config):
-
+		print('setting gpio pins')
 		for pin in config['gpio']:
-			if pin['type'] == 'OUT':
-				pin_type = GPIO.OUT
-			elif pin['type'] == 'IN':
-				pin_type = GPIO.IN
+			if len(pin['type']) > 1:
+				GPIO.setup(pin['pins'], pin['type'])
+			else:
+				GPIO.setup(pin['pins'], pin['types'][0], pin['types'][1])
 
-			GPIO.setup(pin['pins'], pin_type)
+			if pin['name'] == 'BTN_CHANGE_MODE':
+				self._button_id = config['gpio'].index(pin)
+
 			self._pins.append(pin['pins'])
 
 		self._click_speed = config['core']['click_speed']
 
+		# print('setting io events..')
+		# GPIO.add_event_detect(pin[self._button_id]['pin'][0], 
+		# 						GPIO.FALLING, callback=IO._cb, 
+		# 						bouncetime=300)
 		return 0
 
-	def wait_for_command(self):
-		
+	def _cb(self):
+		print('button pressed')
 
-		GPIO.read(x)
+	def wait_for_command(self, transition_method):
+		GPIO.wait_for_edge(config['gpio'][self._button_id], 
+			GPIO.FALLING, 
+			bouncetime=300)
+
+		transition_method()
+		return 0
+
+
 
 if __name__ == '__main__':
 	APP_DIRECTORY 	= 'sb-robot/sb-robot'
@@ -52,17 +69,17 @@ if __name__ == '__main__':
 					"MAC": ["64:A2:F9:2F:6A:9B"]},
 		"motorGLOBAL":{	"f_PWM": 200,},
 		"gpio"	: [{ 	"name": "leftMotor",
-						"type": "OUT",
+						"type": [GPIO.OUT, GPIO.PUD_UP],
 						"pins": [12, 20, 16]}, 
 					{ 	"name": "rightMotor",
-						"type": "OUT",
+						"type": [GPIO.OUT, GPIO.PUD_UP],
 						"pins": [13, 26, 19]},
-					{ 	"name": "StatusLED_BT",
-						"type": "OUT",
+					{ 	"name": "LED_STATUS_BT",
+						"type": [GPIO.OUT, GPIO.PUD_UP],
 						"pins": [14]},
-				    {	"pins": [15],
-    					"name": "BTN_CHANGE_MODE",
-      					"type": "IN"}]}
+				    {	"name": "BTN_CHANGE_MODE",
+      					"type": [GPIO.IN],
+      					"pins": [15]}]}
 
 
 	Configuration.exportConfig(config_in, os.path.join(DATA_DIRECTORY, CONFIG_FILE))
