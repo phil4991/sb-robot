76581fb852293b9a18373d851069bdb010fd45c4
phil4991 <philipp_gehring@yahoo.de>
2019-09-17 20:19:46 +0200

    added angle plot

--- tests/daq_plot.m
+++ tests/daq_plot.m
@@ -1,7 +1,8 @@
 clear all; clc
 %% import
-in = dlmread('../data/IMU_data.txt')
+in = dlmread('../data/IMU_data.txt', ' ', 2);
 channel = [2,3,4]
+t = in(:, 1);
 
 %% calc
 T = in(end,1)-in(1,1)
@@ -10,14 +11,18 @@ freqs = T./k;
 f = fft(in(:, channel))/length(in(:, channel));
 
 %% plots
-subplot(2, 1, 1)
-plot(in(:, channel))
+subplot(3, 1, 1)
+plot(t, in(:, channel))
 title('Acceleration of IMU')
 legend('a_x','a_y','a_z')
 xlabel('number of samples')
 grid on
 
-subplot(2, 1, 2)
+subplot(3, 1, 2)
 plot(freqs,f)
 title('Frequency spectrum of acceleration')
+
+subplot(3, 1, 3)
+plot(t, in(:, 5))
+
 saveas(gcf, 'fig/imu_accel_data.svg')
